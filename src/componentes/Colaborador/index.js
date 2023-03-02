3d0333a52ea1fdcf53c858f9401f721f87d1ae1b
Daniel Marcos Pires <89848369+DanielMarcosPires@users.noreply.github.com>
2023-03-02 13:09:55 -0300

    O término sobre components em react

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,40 @@
-import './colaborador.css'
+import "./colaborador.css";
+import { GrClose } from "react-icons/gr";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+  function favoritar() {
+    aoFavoritar(colaborador.id);
+  }
 
-export default Colaborador
+  const propsFavorito = {
+    size: 25,
+    onclick: favoritar,
+  };
+
+  return (
+    <div className="colaborador">
+      <GrClose
+        size={25}
+        className="deletar"
+        onClick={() => aoDeletar(colaborador.id)}
+      />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+        <div className="favoritar">
+          {colaborador.favorito ? (
+            <MdFavorite {...propsFavorito} color="#ff0000" />
+          ) : (
+            <MdFavoriteBorder {...propsFavorito} color="#ff0000" />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
